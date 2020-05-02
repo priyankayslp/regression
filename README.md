d3ab02ee08172ad357bfccc74e372dc6e5f624e5
PRAYANKA <pushpapriyanka493@gmail.com>
2020-05-02 19:46:07 +0530

    linear regression

---
+++ README.md
@@ -0,0 +1,10 @@
+read the data using pandas
+plot the scatter plot to visualise the relation between ENGINESIZE AND CO2EMISSIONS
+split train data using numpy:80%
+split test dat : 20%
+fit the data into linear regression model using scikit learn
+find the coefficient and intercept 
+and plot the fit line over the data 
+calculate the errors like mean absolute and mean square
+and also calculate R2_Score which is a measure of ow close the data are to the fitted regression line.Higher the value good is the model 
+
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import pylab as pl
 import numpy as np
-from sklearn import linear_model
 
-df = pd.read_csv("FuelConsumption.csv")
+df = pd.read_csv("data/FuelConsumption.csv")
 # take a look at the dataset
 df.head()
+df.describe()
 cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_CITY','FUELCONSUMPTION_HWY','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
 cdf.head(9)
-plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
+viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_HWY']]
+viz.hist()
+plt.show()
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='red')
 plt.xlabel("Engine size")
 plt.ylabel("Emission")
 plt.show()
@@ -20,11 +22,25 @@ plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
 plt.xlabel("Engine size")
 plt.ylabel("Emission")
 plt.show()
-
-
+from sklearn import linear_model
 regr = linear_model.LinearRegression()
-x = np.asanyarray(train[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
-y = np.asanyarray(train[['CO2EMISSIONS']])
-regr.fit (x, y)
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit (train_x, train_y)
 # The coefficients
 print ('Coefficients: ', regr.coef_)
+print ('Intercept: ',regr.intercept_)
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.savefig('plot.png')
+from sklearn.metrics import r2_score
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_hat = regr.predict(test_x)
+
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_hat - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_hat - test_y) ** 2))
+# a popular metric for accuracy of your model. It represents how close the data are to the fitted regression line. The higher the R-squared, the better the model fits your data
+print("R2-score: %.2f" % r2_score(test_y_hat , test_y) )

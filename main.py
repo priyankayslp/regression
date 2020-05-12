2f16dca6f56dc4ac0f2ea15834e44146ff259e0c
PRAYANKA <pushpapriyanka493@gmail.com>
2020-05-12 23:46:50 +0530

    polynomial linear regression

--- main.py
+++ main.py
@@ -3,43 +3,49 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv("data/FuelConsumption.csv")
-# take a look at the dataset
 df.head()
-df.describe()
-cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_CITY','FUELCONSUMPTION_HWY','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
 cdf.head(9)
-viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_HWY']]
-viz.hist()
-plt.show()
-plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='red')
-plt.xlabel("Engine size")
-plt.ylabel("Emission")
-plt.show()
+
+plt.scatter(cdf.ENGINESIZE,cdf.CO2EMISSIONS,color='blue')
+plt.xlabel('Engine Size')
+plt.ylabel('CO2 Emissions')
+# plt.show()
+
 msk = np.random.rand(len(df)) < 0.8
 train = cdf[msk]
 test = cdf[~msk]
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+
+poly = PolynomialFeatures(degree=2)
+train_x_poly = poly.fit_transform(train_x)
+
+regr=linear_model.LinearRegression()
+regr.fit(train_x_poly,train_y)
+print('Coffecients',regr.coef_)
+print('Intercept',regr.intercept_)
+
+
 plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+XX = np.arange(0.0, 10.0, 0.1)
+yy = regr.intercept_[0]+ regr.coef_[0][1]*XX+ regr.coef_[0][2]*np.power(XX, 2)
+plt.plot(XX, yy, '-r' )
 plt.xlabel("Engine size")
 plt.ylabel("Emission")
 plt.show()
-from sklearn import linear_model
-regr = linear_model.LinearRegression()
-train_x = np.asanyarray(train[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
-train_y = np.asanyarray(train[['CO2EMISSIONS']])
-regr.fit (train_x, train_y)
-# The coefficients
-from sklearn.metrics import r2_score
-test_x = np.asanyarray(test[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
-test_y = np.asanyarray(test[['CO2EMISSIONS']])
-test_y_hat = regr.predict(test_x)
 
-print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_hat - test_y)))
-print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_hat - test_y) ** 2))
-# a popular metric for accuracy of your model. It represents how close the data are to the fitted regression line. The higher the R-squared, the better the model fits your data
-r2=r2_score(test_y, test_y_hat, multioutput="uniform_average")
-print("R2-score: %.4f" % r2 )
-n=len(train_y)-1
-k=train_x.shape[1]
-r2_adjusted=1-(((1-r2)*(n-1))/(n-k-1))
-print("Adjusted R2 : %.4f" % r2_adjusted)
-print('Variance score: %.4f' % regr.score(test_x, test_y))
+
+from sklearn.metrics import r2_score
+
+test_x_poly = poly.fit_transform(test_x)
+test_y_ = regr.predict(test_x_poly)
+
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(test_y , test_y_) )
